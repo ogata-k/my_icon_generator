09682a1821220d63f9192f97734f4a6c5604c8e9
ogata-k <ogtkzk712@gmail.com>
2019-12-25 22:41:52 +0900

    CLI化完了

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,50 @@
+use clap::{App, Arg};
 use raqote::*;
 use std::fs;
 use std::io;
 use std::path::Path;
 
 fn main() -> Result<(), io::Error> {
-    run("image/icon.png", 3000)
+    let matches = App::new("icon-gen")
+        .version("1.0")
+        .author("ogata-k. <ogtkzk712@gmail.com>")
+        .about("my icon generator. but png only.")
+        .arg(
+            Arg::with_name("size")
+                .short("s")
+                .long("size")
+                .default_value("2000")
+                .help("image size")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("file")
+                .short("f")
+                .long("file")
+                .default_value("icon.png")
+                .help("Sets the input file to use"),
+        )
+        .get_matches();
+    let size: u16 = matches
+        .value_of("size")
+        .expect("不正なサイズです。")
+        .parse()
+        .expect("fail parse size");
+    let path: &str = matches.value_of("file").expect("不正なファイルパスです。");
+
+    run(path, size)
 }
 
 fn run<P: AsRef<std::path::Path>>(path: P, size: u16) -> Result<(), io::Error> {
     let path: &Path = path.as_ref();
-    let mut split_path: Vec<String> = path.to_string_lossy().split("/").map(|s| s.to_string()).collect();
-    split_path.pop();
+    let mut split_path: Vec<String> = path
+        .to_string_lossy()
+        .split("/")
+        .map(|s| s.to_string())
+        .collect();
+    split_path
+        .pop()
+        .expect("ファイル名を取得できませんでした。");
     let dir_path: String = split_path.join("/");
     fs::create_dir_all(&dir_path)?;
 
@@ -19,7 +53,7 @@ fn run<P: AsRef<std::path::Path>>(path: P, size: u16) -> Result<(), io::Error> {
 
 fn create_icon(size: u16) -> DrawTarget {
     let width: u16 = size;
-    let height : u16 = size;
+    let height: u16 = size;
     let mut dt = DrawTarget::new(width.into(), height.into());
 
     // back ground
@@ -28,31 +62,39 @@ fn create_icon(size: u16) -> DrawTarget {
     dt.fill(
         &pb.finish(),
         &Source::Solid(SolidSource::from_unpremultiplied_argb(255, 250, 250, 250)),
-            &DrawOptions::new()
+        &DrawOptions::new(),
     );
 
     // outline
     let lup: Point = Point::new(width as f32 / 10., 2. * (height as f32 / 9.));
     let view_size: Point = Point::new(7. * (width as f32 / 10.), 2. * (height as f32 / 5.));
-    let rbp : Point = Point::new(lup.x + view_size.x, lup.y + view_size.y);
+    let rbp: Point = Point::new(lup.x + view_size.x, lup.y + view_size.y);
     let mut pb = PathBuilder::new();
-    pb.move_to((5. * lup.x + rbp.x) / 6. , 99. * (rbp.y / 100.));
-    pb.quad_to((9. * lup.x + rbp.x) / 10.,49. * (rbp.y / 50.), lup.x, rbp.y);
+    pb.move_to((5. * lup.x + rbp.x) / 6., 99. * (rbp.y / 100.));
+    pb.quad_to(
+        (9. * lup.x + rbp.x) / 10.,
+        49. * (rbp.y / 50.),
+        lup.x,
+        rbp.y,
+    );
     pb.line_to(lup.x, 99. * (rbp.y / 100.));
     pb.cubic_to(
-        0.4 * width as f32, 0.9 * lup.y,
-        1.55 * rbp.x, -0.05 * lup.y,
-        (3. * lup.x + 5. * rbp.x) / 8., 101. * (rbp.y / 100.)
+        0.4 * width as f32,
+        0.9 * lup.y,
+        1.55 * rbp.x,
+        -0.05 * lup.y,
+        (3. * lup.x + 5. * rbp.x) / 8.,
+        101. * (rbp.y / 100.),
     );
     dt.stroke(
         &pb.finish(),
         &Source::Solid(SolidSource::from_unpremultiplied_argb(255, 0, 0, 0)),
-        &StrokeStyle{
+        &StrokeStyle {
             width: 0.01 * size as f32,
             cap: LineCap::Round,
             ..StrokeStyle::default()
         },
-        &DrawOptions::new()
+        &DrawOptions::new(),
     );
 
     // inner ellipse
@@ -62,14 +104,14 @@ fn create_icon(size: u16) -> DrawTarget {
     pb.move_to(mid_x, from_y);
     pb.line_to(mid_x, 1.02 * from_y);
     dt.stroke(
-      &pb.finish(),
+        &pb.finish(),
         &Source::Solid(SolidSource::from_unpremultiplied_argb(255, 0, 0, 0)),
-      &StrokeStyle{
-          width: 0.06 * size as f32,
-          cap: LineCap::Round,
-          ..StrokeStyle::default()
-      },
-      &DrawOptions::new()
+        &StrokeStyle {
+            width: 0.06 * size as f32,
+            cap: LineCap::Round,
+            ..StrokeStyle::default()
+        },
+        &DrawOptions::new(),
     );
 
     return dt;
